c9b3a29ebffd59f9da1def80207e9afd0e1cb576
tandy1000 <tandy1000@cock.li>
2021-08-01 21:57:06 +0100

    restore prologue

--- src/listen2gether.nim
+++ src/listen2gether.nim
@@ -2,6 +2,11 @@ import os
 import sources/[lb, lfm]
 #import models
 
+import prologue
+import prologue/middlewares/utils
+import prologue/middlewares/staticfile
+import ./routes/urls
+
 when isMainModule:
   # let
   #   #db = openDbConn()
@@ -14,3 +19,18 @@ when isMainModule:
   #   listenPayload = syncListenBrainz.getCurrentTrack(mirroredUser)
   #   listenSubmission = syncListenBrainz.listenTrack(listenPayload, listenType="single")
   # #db.insertListen(listenPayload.listens[0])
+
+  let
+    env = loadPrologueEnv(".env")
+    settings = newSettings(appName = env.getOrDefault("appName", "Prologue"),
+                  debug = env.getOrDefault("debug", true),
+                  port = Port(env.getOrDefault("port", 8888)),
+                  secretKey = env.getOrDefault("secretKey", "a")
+      )
+
+  var app = newApp(settings = settings)
+
+  app.use(staticFileMiddleware(env.getOrDefault("staticDir", "static")))
+  app.use(debugRequestMiddleware())
+  app.addRoute(urls.urlPatterns, "")
+  app.run()
