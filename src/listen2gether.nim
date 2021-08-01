d4364a13e4ae093029aa8b8a1c958ffca5e61872
tandy1000 <tandy1000@cock.li>
2021-08-01 23:16:03 +0100

    setup prologue

--- src/listen2gether.nim
+++ src/listen2gether.nim
@@ -1,31 +1,29 @@
-import os
-import sources/[lb, lfm]
-#import models
-
 import prologue
 import prologue/middlewares/utils
 import prologue/middlewares/staticfile
+
+import sources/[lb, lfm]
+#import models
 import urls
 
-when isMainModule:
-  # let
-  #   #db = openDbConn()
-  #   clientUser = newUser(username = lbUser, lbToken = some(lbToken))
-  #   mirroredUser = newUser(username = lbUser1)
-  #   syncListenBrainz = newSyncListenBrainz(get(clientUser.lbToken))
-  # #db.insertTables()
-  # syncListenBrainz.validateLbToken(get(clientUser.lbToken))
-  # let
-  #   listenPayload = syncListenBrainz.getCurrentTrack(mirroredUser)
-  #   listenSubmission = syncListenBrainz.listenTrack(listenPayload, listenType="single")
-  # #db.insertListen(listenPayload.listens[0])
+# let
+#   #db = openDbConn()
+#   clientUser = newUser(username = lbUser, lbToken = some(lbToken))
+#   mirroredUser = newUser(username = lbUser1)
+#   syncListenBrainz = newSyncListenBrainz(get(clientUser.lbToken))
+# #db.insertTables()
+# syncListenBrainz.validateLbToken(get(clientUser.lbToken))
+# let
+#   listenPayload = syncListenBrainz.getCurrentTrack(mirroredUser)
+#   listenSubmission = syncListenBrainz.listenTrack(listenPayload, listenType="single")
+# #db.insertListen(listenPayload.listens[0])
 
-  let settings = newSettings(appName = "Listen2gether",
-                             debug = true,
-                             port = Port(8080))
-  var app = newApp(settings = settings)
+let settings = newSettings(appName = "Listen2gether",
+                           debug = true,
+                           port = Port(8080))
+var app = newApp(settings = settings)
 
-  app.use(staticFileMiddleware("templates"))
-  app.use(debugRequestMiddleware())
-  app.addRoute(urls.urlPatterns, "")
-  app.run()
+app.use(staticFileMiddleware("templates"))
+app.use(debugRequestMiddleware())
+app.addRoute(urls.urlPatterns, "")
+app.run()
