2d50cc169eb0f22dae6c358fd89a057781506070
tandy1000 <tandy1000@cock.li>
2021-08-05 12:46:43 +0100

    use container div to avoid setting rules on main div

--- src/templates/home.nim
+++ src/templates/home.nim
@@ -11,13 +11,14 @@ proc onUsernameEnter(ev: Event; n: VNode) =
 
 proc makeMain(): Vnode =
   result = buildHtml(main()):
-    tdiv(id = "username", class = "textbox"):
-      input(`type` = "text", class = "textinput", id = "username_input", placeholder = "Enter their Username", onkeyupenter = onUsernameEnter)
-      label(class = "switch"):
-        input(`type` = "checkbox", id = "service_switch", oninput = onServiceToggleClick)
-        span(class = "slider")
-    tdiv(id = "token", class = "textbox"):
-      input(`type` = "text", class = "textinput", id = "token_input", placeholder = "Enter your ListenBrainz token")
+    tdiv(id = "login"):
+      tdiv(id = "username", class = "textbox"):
+        input(`type` = "text", class = "textinput", id = "username_input", placeholder = "Enter their Username", onkeyupenter = onUsernameEnter)
+        label(class = "switch"):
+          input(`type` = "checkbox", id = "service_switch", oninput = onServiceToggleClick)
+          span(class = "slider")
+      tdiv(id = "token", class = "textbox"):
+        input(`type` = "text", class = "textinput", id = "token_input", placeholder = "Enter your ListenBrainz token")
 
 proc createDom(): VNode =
   result = buildHtml(tdiv(class = "grid")):
