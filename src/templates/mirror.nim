d78943765a74e222ee922c6c1330f9a8769658e0
tandy1000 <tandy1000@cock.li>
2021-08-05 12:46:21 +0100

    initial mirror template

--- src/templates/mirror.nim
+++ src/templates/mirror.nim
@@ -3,7 +3,13 @@ import tools
 
 proc makeMain(): Vnode =
   result = buildHtml(main()):
-    tdiv(class = "mirror-grid")
+    tdiv(class = "mirror-grid"):
+      tdiv(id = "chat-container"):
+        p:
+          text "Chat functionality is not ready yet!"
+      tdiv(id = "right-panel"):
+        p:
+          text "You are mirroring!"
 
 proc createDom(): VNode =
   result = buildHtml(tdiv(class = "grid")):
