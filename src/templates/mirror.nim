92817cf9d1b36b3d82d984cd682097ba79a2c1ee
tandy1000 <tandy1000@cock.li>
2021-08-06 20:43:55 +0100

    mirror template listen history example

--- src/templates/mirror.nim
+++ src/templates/mirror.nim
@@ -3,13 +3,36 @@ import tools
 
 proc makeMain(): Vnode =
   result = buildHtml(main()):
-    tdiv(class = "mirror-grid"):
-      tdiv(id = "chat-container"):
-        p:
-          text "Chat functionality is not ready yet!"
-      tdiv(id = "right-panel"):
-        p:
-          text "You are mirroring!"
+    tdiv(class = "listens"):
+      ul:
+        li:
+          img(src = "src/templates/assets/listening.gif", id = "scrobble", class = "icon")
+          tdiv(id = "listen-details"):
+            tdiv(id = "track-details"):
+              p(id = "track-name"): text "Ai No Shizuku"
+              p(id = "artist-name"): text "Nobue Kawana"
+            span: text "24 Jul, 3:58 pm"
+        li:
+          img(src = "src/templates/assets/listened.svg", id = "listened", class = "icon")
+          tdiv(id = "listen-details"):
+            tdiv(id = "track-details"):
+              p(id = "track-name"): text "Ai No Shizuku"
+              p(id = "artist-name"): text "Nobue Kawana"
+            span: text "24 Jul, 3:58 pm"
+        li:
+          img(src = "src/templates/assets/listened.svg", id = "listened", class = "icon")
+          tdiv(id = "listen-details"):
+            tdiv(id = "track-details"):
+              p(id = "track-name"): text "Ai No Shizuku"
+              p(id = "artist-name"): text "Nobue Kawana"
+            span: text "24 Jul, 3:58 pm"
+        li:
+          img(src = "src/templates/assets/listened.svg", id = "listened", class = "icon")
+          tdiv(id = "listen-details"):
+            tdiv(id = "track-details"):
+              p(id = "track-name"): text "Ai No Shizuku"
+              p(id = "artist-name"): text "Nobue Kawana"
+            span: text "24 Jul, 3:58 pm"
 
 proc createDom(): VNode =
   result = buildHtml(tdiv(class = "grid")):
