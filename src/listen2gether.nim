6d5214f0741b1f4a9ce264a49cb7fa19ff625511
tandy1000 <tandy1000@cock.li>
2021-08-20 21:16:21 +0100

    test refactor

--- src/listen2gether.nim
+++ src/listen2gether.nim
@@ -3,23 +3,26 @@ import prologue / middlewares / staticfile
 
 import sources / [lb, lfm]
 import routes / urls
+import types
 #import models
 
-
-# let
-#   #db = openDbConn()
-#   clientUser = newUser(username = lbUser, lbToken = some(lbToken))
-#   mirroredUser = newUser(username = lbUser1)
-#   syncListenBrainz = newSyncListenBrainz(get(clientUser.lbToken))
-# #db.insertTables()
+let
+  #db = openDbConn()
+  listenBrainzUser = newServiceUser(service = listenBrainz,
+                                    username = "tandy1000")
+  mirroredUser = newUser(services = [listenBrainzUser])
+  #syncListenBrainz = newSyncListenBrainz(mirroredUser.services[listenBrainz].token)
+  # syncLastFM = newSyncLastFM(sessionKey = mirroredUser.services[lastFm].apiKey)
+#db.insertTables()
 # syncListenBrainz.validateLbToken(get(clientUser.lbToken))
-# let
-#   listenPayload = syncListenBrainz.getCurrentTrack(mirroredUser)
-#   listenSubmission = syncListenBrainz.listenTrack(listenPayload, listenType="single")
-# #db.insertListen(listenPayload.listens[0])
+#let
+  #listenPayload = syncListenBrainz.getCurrentTrack(mirroredUser)
+  #listenSubmission = syncListenBrainz.listenTrack(listenPayload, listenType="single")
+# syncLastFM.getRecentTracks(mirroredUser)
+#db.insertListen(listenPayload.listens[0])
 
-var app = newApp(settings = newSettings(appName = "Listen2gether",
-                                        port = Port(8080)))
-app.use(staticFileMiddleware("src/templates"))
-app.addRoute(urls.urlPatterns, "")
-app.run()
+# var app = newApp(settings = newSettings(appName = "Listen2gether",
+#                                         port = Port(8080)))
+# app.use(staticFileMiddleware("src/templates"))
+# app.addRoute(urls.urlPatterns, "")
+# app.run()
