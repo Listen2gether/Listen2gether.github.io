2cf757a3bd14e7f2a500c1f68bfd6fae35c8df8d
tandy1000 <tandy1000@cock.li>
2021-08-26 20:26:46 +0100

    working listenbrainz source test

---
+++ tests/lbTest.nim
@@ -0,0 +1,29 @@
+import std/unittest
+include ../src/sources/lb
+import print
+
+suite "ListenBrainz source":
+  setup:
+    let
+      jsonEx = readFile("tests/lbEx.json")
+      listenPayloadJson = parseJson(jsonEx)
+      listenPayload = newListenPayload(
+        count = 1,
+        latestListenTs = none(int64),
+        listens = @[newListen(trackMetadata = newTrackMetadata(
+          trackName = "Mais Que Amor",
+          artistName = "Marcos Valle",
+          releaseName = "Marcos Valle",
+          additionalInfo = some(newAdditionalInfo(
+            trackMbid = "9f7b866c-49e8-4126-96de-f42cab2cbd4f",
+            recordingMbid = "c0671f4d-b9c3-432c-909c-4e27c734c950",
+            releaseMbid = "21484f51-09b4-4c6b-855d-a6f3874a23ce",
+            artistMbids = @["1ba347eb-58e8-45d6-b7e3-09873dc2506a"]))))],
+        playingNow = some(true),
+        userId = "test")
+
+  test "jsony - ListenPayload":
+    let
+      payloadJson = listenPayloadJson["payload"]
+      payloadObj = fromJson($payloadJson, ListenPayload)
+    check listenPayload == payloadObj
