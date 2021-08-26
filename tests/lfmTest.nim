ff495f1f19a79f6ee888a92d5615aad39d7bc1d8
tandy1000 <tandy1000@cock.li>
2021-08-26 20:27:15 +0100

    initial jsony lastfm source test - not working

--- tests/lfmTest.nim
+++ tests/lfmTest.nim
@@ -1,20 +1,21 @@
 import std/unittest
 include ../src/sources/lfm
 
-suite "jsony":
+suite "Last.FM source":
   setup:
     let
-      jsonEx = readFile("tests/ex.json")
-      lastFmRecentTracksJson = parseJson(jsonEx)["recenttracks"]
-      lastFmRecentTracks = newFMTrack(artist = newFMObject(mbid = "", text = "Ryuichi Sakamoto"),
-                                      album = newFMObject(mbid = "0f10b982-1be7-4eb9-94fe-0376ea99a980", text = "Esperanto"),
-                                      mbid = "",
-                                      name = "A Wongga Dance Song",
-                                      url = "https://www.last.fm/music/Ryuichi+Sakamoto/_/A+Wongga+Dance+Song",
-                                      attr = some(newAttributes(nowplaying = true)))
+      jsonEx = readFile("tests/lfmEx.json")
+      recentTracksJson = parseJson(jsonEx)["recenttracks"]
+      recentTracks = newFMTrack(
+        artist = newFMObject(mbid = "", text = "Ryuichi Sakamoto"),
+        album = newFMObject(mbid = "0f10b982-1be7-4eb9-94fe-0376ea99a980", text = "Esperanto"),
+        mbid = "",
+        name = "A Wongga Dance Song",
+        url = "https://www.last.fm/music/Ryuichi+Sakamoto/_/A+Wongga+Dance+Song",
+        attr = some(newAttributes(nowplaying = true)))
 
-  test "lastFmRecentTracks":
+  test "jsony - FMTrack":
     let
-      trackJson = $lastFmRecentTracksJson["track"][0]
+      trackJson = $recentTracksJson["track"][0]
       trackObj = fromJson(trackJson, FMTrack)
-    check lastFmRecentTracks == trackObj
+    check recentTracks == trackObj
