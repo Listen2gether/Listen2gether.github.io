128862c9c8e44dacfddf675f78bcb705b09cefd1
tandy1000 <tandy1000@cock.li>
2021-08-30 18:49:42 +0100

    fix jsony parse test

--- tests/lfmTest.nim
+++ tests/lfmTest.nim
@@ -1,21 +1,23 @@
 import std/unittest
 include ../src/sources/lfm
+import print
 
 suite "Last.FM source":
   setup:
     let
       jsonEx = readFile("tests/lfmEx.json")
-      recentTracksJson = parseJson(jsonEx)["recenttracks"]
-      recentTracks = newFMTrack(
+      jsonNode = parseJson(jsonEx)
+
+  test "jsony - FMTrack":
+    let
+      recentTracksJson = jsonNode["RecentTracks"]
+      fmTrack = newFMTrack(
         artist = newFMObject(mbid = "", text = "Ryuichi Sakamoto"),
         album = newFMObject(mbid = "0f10b982-1be7-4eb9-94fe-0376ea99a980", text = "Esperanto"),
         mbid = "",
         name = "A Wongga Dance Song",
         url = "https://www.last.fm/music/Ryuichi+Sakamoto/_/A+Wongga+Dance+Song",
         attr = some(newAttributes(nowplaying = true)))
-
-  test "jsony - FMTrack":
-    let
-      trackJson = $recentTracksJson["track"][0]
-      trackObj = fromJson(trackJson, FMTrack)
-    check recentTracks == trackObj
+      fmTrackJson = $recentTracksJson["recenttracks"]["track"][0]
+      fmTrackObj = fmTrackJson.fromJson(FMTrack)
+    check fmTrack == fmTrackObj
