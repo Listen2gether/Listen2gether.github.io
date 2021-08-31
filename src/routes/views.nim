7472e59532a02a5fe39d870c2870cff067810194
tandy1000 <tandy1000@cock.li>
2021-09-01 00:18:40 +0100

    create mirror page view

--- src/routes/views.nim
+++ src/routes/views.nim
@@ -1,7 +1,28 @@
 import prologue
+import ../templates/mirror
+import ../sources/[lb, lfm]
+import ../types
 
 proc home*(ctx: Context) {.async.} =
   resp readFile("src/templates/home.html")
 
 proc mirror*(ctx: Context) {.async.} =
-  resp readFile("src/templates/mirror.html")
+  let
+    serviceParam = ctx.getPathParams("service")
+    usernameParam = ctx.getPathParams("username")
+  var
+    user: User = newUser()
+    service: Service
+  case serviceParam:
+    of "listenbrainz":
+      service = listenBrainzService
+      let syncListenBrainz = newSyncListenBrainz()
+      user.services[listenBrainzService].username = usernameParam
+      syncListenBrainz.updateUser(user)
+    of "lastfm":
+      service = lastFmService
+      let syncLastFM = newSyncLastFM()
+      user.services[lastFmService].username = usernameParam
+      syncLastFM.updateUser(user)
+  echo user.listenHistory
+  resp htmlResponse(mirrorPage(ctx, service, user))
