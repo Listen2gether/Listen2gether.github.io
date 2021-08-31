21fc3aa8d6007d6eb4db307929e1f44536c7597b
tandy1000 <tandy1000@cock.li>
2021-09-01 00:18:11 +0100

    mirror page gets data from sources

--- src/templates/mirror.nim
+++ src/templates/mirror.nim
@@ -1,51 +1,48 @@
-import karax / [karax, karaxdsl, vdom]
-import tools
+import karax/[karaxdsl, vdom]
+import prologue
+import share
+import ../types
 
-proc makeMain(): Vnode =
+proc mainSection(service: Service, user: User): Vnode =
+  var username: string
+  case service:
+    of listenBrainzService:
+      username = user.services[listenBrainzService].username
+    of lastFmService:
+      username = user.services[lastFmService].username
   result = buildHtml(main()):
-    verbatim("<div id = 'mirror'><p>You are mirroring <a href='test'>X</a>!</p></div>")
+    verbatim("<div id = 'mirror'><p>You are mirroring <a href='test'>" & username & "</a>!</p></div>")
     tdiv(class = "listens"):
       ul:
-        li(class = "listen"):
-          img(src = "src/templates/assets/listening.svg")
-          tdiv(id = "listen-details"):
-            tdiv(id = "track-details"):
-              p(id = "track-name"): text "Ai No Shizuku"
-              p(id = "artist-name"): text "Nobue Kawana"
-            span: text "24 Jul, 3:58 pm"
-        li(class = "listen"):
-          img(src = "src/templates/assets/listened.svg")
-          tdiv(id = "listen-details"):
-            tdiv(id = "track-details"):
-              p(id = "track-name"): text "Ai No Shizuku"
-              p(id = "artist-name"): text "Nobue Kawana"
-            span: text "24 Jul, 3:58 pm"
-        li(class = "listen"):
-          img(src = "src/templates/assets/listened.svg")
-          tdiv(id = "listen-details"):
-            tdiv(id = "track-details"):
-              p(id = "track-name"): text "Ai No Shizuku"
-              p(id = "artist-name"): text "Nobue Kawana"
-            span: text "24 Jul, 3:58 pm"
-        li(class = "listen"):
-          img(src = "src/templates/assets/listened.svg")
-          tdiv(id = "listen-details"):
-            tdiv(id = "track-details"):
-              p(id = "track-name"): text "Ai No Shizuku"
-              p(id = "artist-name"): text "Nobue Kawana"
-            span: text "24 Jul, 3:58 pm"
-        li(class = "listen"):
-          img(src = "src/templates/assets/pre-mirror-listen.svg")
-          tdiv(id = "listen-details"):
-            tdiv(id = "track-details"):
-              p(id = "track-name"): text "Ai No Shizuku"
-              p(id = "artist-name"): text "Nobue Kawana"
-            span: text "24 Jul, 3:58 pm"
-            
-proc createDom(): VNode =
-  result = buildHtml(tdiv(class = "grid")):
-    makeHeader()
-    makeMain()
-    makeFooter()
+        if isSome(user.playingNow):
+          li(class = "listen"):
+            img(src = "src/templates/assets/listening.svg")
+            tdiv(id = "listen-details"):
+              tdiv(id = "track-details"):
+                p(id = "track-name"):
+                  text get(user.playingNow).trackName
+                p(id = "artist-name"):
+                  text get(user.playingNow).artistName
+              span:
+                text "Playing Now"
+        for track in user.listenHistory:
+          li(class = "listen"):
+            img(src = "src/templates/assets/listened.svg")
+            tdiv(id = "listen-details"):
+              tdiv(id = "track-details"):
+                p(id = "track-name"):
+                  text track.trackName
+                p(id = "artist-name"):
+                  text track.artistName
+              span:
+                text $get(track.listenedAt)
 
-setRenderer createDom
+proc mirrorPage*(ctx: Context, service: Service, user: User): string =
+  let vnode = buildHtml(html):
+    head()
+    body:
+      tdiv(class = "grid"):
+        headerSection()
+        mainSection(service, user)
+        footerSection()
+  result = $vnode
