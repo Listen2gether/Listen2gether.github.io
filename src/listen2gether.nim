9fda511d577c405b3784344119990964c8cc59b5
tandy1000 <tandy1000@cock.li>
2021-09-01 17:53:19 +0100

    remove cruft

--- src/listen2gether.nim
+++ src/listen2gether.nim
@@ -1,30 +1,9 @@
 import prologue
-import prologue / middlewares / staticfile
+import prologue/middlewares/staticfile
+import routes/urls
 
-import sources / [lb, lfm]
-import routes / urls
-import types
-#import models
-
-let
-  #db = openDbConn()
-  lastFmUser = newServiceUser(service = lastFm,
-                              username = "tandy1000")
-  listenBrainzUser = newServiceUser(service = listenBrainz,
-                                    username = "tandy1000")
-  mirroredUser = newUser(services = [lastFmUser, listenBrainzUser])
-  #syncListenBrainz = newSyncListenBrainz(mirroredUser.services[listenBrainz].token)
-  syncLastFM = newSyncLastFM(apiKey = mirroredUser.services[lastFm].apiKey)
-#db.insertTables()
-# syncListenBrainz.validateLbToken(get(clientUser.lbToken))
-#let
-  #listenPayload = syncListenBrainz.getCurrentTrack(mirroredUser)
-  #listenSubmission = syncListenBrainz.listenTrack(listenPayload, listenType="single")
-# syncLastFM.getRecentTracks(mirroredUser)
-#db.insertListen(listenPayload.listens[0])
-
-# var app = newApp(settings = newSettings(appName = "Listen2gether",
-#                                         port = Port(8080)))
-# app.use(staticFileMiddleware("src/templates"))
-# app.addRoute(urls.urlPatterns, "")
-# app.run()
+let settings = newSettings(appName = "Listen2gether", port = Port(8080))
+var app = newApp(settings = settings)
+app.use(staticFileMiddleware("src/templates"))
+app.addRoute(urls.urlPatterns, "")
+app.run()
