d51b027b3dc3e8177e6ee5e5ffd13e1b26749d47
tandy1000 <tandy1000@cock.li>
2021-09-13 18:58:57 +0100

    initial browser js support

--- src/templates/mirror.nim
+++ src/templates/mirror.nim
@@ -1,18 +1,49 @@
-import karax/[karaxdsl, vdom]
-import prologue, times
+import std/[times, strutils, options]
+import karax/[karax, karaxdsl, vdom, kdom]
+import listenbrainz
+import ../sources/[lb]
+# , lfm]
 import share
 import ../types
 
+proc getPathParams(): (Service, string) = 
+  var service: Service
+  let
+    pathSeq = split($window.location.pathname, '/')
+    serviceString = pathSeq[2]
+    username = pathSeq[3]
+  case serviceString:
+    of "listenbrainz":
+      service = listenBrainzService
+    of "lastfm":
+      service = lastFmService
+  result = (service, username)
 
-proc mainSection(service: Service, user: User): Vnode =
-  var username, userUrl: string
+proc createUser(): (User, string, string) = 
+  let
+    pathParams = getPathParams()
+    service = pathParams[0]
+    username = pathParams[1]
+  var
+    user: User = newUser()
+    userUrl: string
   case service:
     of listenBrainzService:
-      username = user.services[listenBrainzService].username
+      user.services[listenBrainzService].username = username
       userUrl = user.services[listenBrainzService].baseUrl & username
+      let asyncListenBrainz = newAsyncListenBrainz()
+      let update = asyncListenBrainz.updateUser(user)
     of lastFmService:
-      username = user.services[lastFmService].username
+      user.services[lastFmService].username = username
       userUrl = user.services[lastFmService].baseUrl & username
+  result = (user, username, userUrl)
+
+proc mainSection(): Vnode =
+  let
+    userTup = createUser()
+    user = userTup[0]
+    username = userTup[1]
+    userUrl = userTup[2]
   result = buildHtml(main()):
     verbatim("<div id = 'mirror'><p>You are mirroring <a href='" & userUrl & "'>" & username & "</a>!</p></div>")
     tdiv(class = "listens"):
@@ -40,17 +71,18 @@ proc mainSection(service: Service, user: User): Vnode =
               span(title = fromUnix(get(track.listenedAt)).format("dd/MM/yy")):
                 text fromUnix(get(track.listenedAt)).format("HH:mm")
 
-proc mirrorPage*(ctx: Context, service: Service, user: User): string =
+proc createDom*(): VNode =
   let
     head = head()
     header = headerSection()
-    main = mainSection(service, user)
+    main = mainSection()
     footer = footerSection()
-    vnode = buildHtml(html):
-      head
-      body:
-        tdiv(class = "grid"):
-          header
-          main
-          footer
-  result = $vnode
+  result = buildHtml(html):
+    head
+    body:
+      tdiv(class = "grid"):
+        header
+        main
+        footer
+
+setRenderer createDom
