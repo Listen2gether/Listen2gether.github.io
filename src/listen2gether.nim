ba9f36931fe408add01d0f76384bd4737a070b1c
tandy1000 <tandy1000@cock.li>
2021-09-13 19:00:14 +0100

    add newline

--- src/listen2gether.nim
+++ src/listen2gether.nim
@@ -1,7 +1,7 @@
 import prologue
 import prologue/middlewares/staticfile
 import routes/urls
-
+   
 let settings = newSettings(appName = "Listen2gether", port = Port(8080))
 var app = newApp(settings = settings)
 app.use(staticFileMiddleware("src/templates"))
