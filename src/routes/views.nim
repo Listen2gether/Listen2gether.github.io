d098a3588b1ea61b33598ca6f65193e03d0816b6
tandy1000 <tandy1000@cock.li>
2021-09-15 19:17:19 +0100

    implement mirror view for listenbrainz

--- src/routes/views.nim
+++ src/routes/views.nim
@@ -1,5 +1,7 @@
-import prologue, listenbrainz
-import ../sources/[lb]
+import karax/localstorage
+import prologue, listenbrainz, jsony
+import ../sources/[lb, lbTypes]
+import ../sources/utils
 # , lfm]
 import ../types
 
@@ -7,4 +9,24 @@ proc home*(ctx: Context) {.async.} =
   resp readFile("src/templates/home.html")
 
 proc mirror*(ctx: Context) {.async.} =
+  let
+    serviceParam = ctx.getPathParams("service")
+    usernameParam = ctx.getPathParams("username")
+    userID = serviceParam & usernameParam
+  var
+    user: User = newUser()
+    service: Service
+  case serviceParam:
+    of "listenbrainz":
+      service = listenBrainzService
+      let asyncListenBrainz = newAsyncListenBrainz()
+      user.services[listenBrainzService].username = usernameParam
+      discard asyncListenBrainz.updateUser(user)
+      setItem(userID, toJson(user.listenHistory))
+    # of "lastfm":
+    #   service = lastFmService
+    #   let asyncLastFM = newAsyncLastFM()
+    #   user.services[lastFmService].username = usernameParam
+    #   discard asyncLastFM.updateUser(user)
+    #   setItem(userID, toJson(user.listenHistory))
   resp readFile("src/templates/mirror.html")
