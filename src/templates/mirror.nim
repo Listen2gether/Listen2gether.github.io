5422184bdc6f628217e1d644eef3581f4e92e1ca
tandy1000 <tandy1000@cock.li>
2021-09-15 19:18:00 +0100

    temporary mirror view fix

--- src/templates/mirror.nim
+++ src/templates/mirror.nim
@@ -1,65 +1,58 @@
-import std/[times, strutils, options]
-import karax/[karax, karaxdsl, vdom, kdom]
-import listenbrainz
-import ../sources/[lb]
+import std/[times, jsffi, strutils, options]
+import karax/[karax, karaxdsl, vdom, kdom, localstorage]
+import listenbrainz, jsony
+import ../sources/[lb, utils]
 # , lfm]
 import share
 import ../types
 
-proc getPathParams(): (Service, string) = 
-  var service: Service
+
+proc getPathParams(): (string, string) = 
   let
     pathSeq = split($window.location.pathname, '/')
     serviceString = pathSeq[2]
     username = pathSeq[3]
-  case serviceString:
-    of "listenbrainz":
-      service = listenBrainzService
-    of "lastfm":
-      service = lastFmService
-  result = (service, username)
+  result = (serviceString, username)
 
-proc createUser(): (User, string, string) = 
+
+proc getListenHistory(): (seq[Track], string, string) = 
   let
     pathParams = getPathParams()
     service = pathParams[0]
     username = pathParams[1]
-  var
-    user: User = newUser()
-    userUrl: string
-  case service:
-    of listenBrainzService:
-      user.services[listenBrainzService].username = username
-      userUrl = user.services[listenBrainzService].baseUrl & username
-      let asyncListenBrainz = newAsyncListenBrainz()
-      let update = asyncListenBrainz.updateUser(user)
-    of lastFmService:
-      user.services[lastFmService].username = username
-      userUrl = user.services[lastFmService].baseUrl & username
-  result = (user, username, userUrl)
+    userId = service & username
+    listenHistory = fromJson($getItem(userId), seq[Track])
+  var userUrl: string
+  # case service:
+  #   of "listenbrainz":
+  #     # userUrl = user.services[listenBrainzService].baseUrl & username
+  #   of "lastfm":
+  #     user.services[lastFmService].username = username
+  #     # userUrl = user.services[lastFmService].baseUrl & username
+  result = (listenHistory, username, userUrl)
 
 proc mainSection(): Vnode =
   let
-    userTup = createUser()
-    user = userTup[0]
-    username = userTup[1]
-    userUrl = userTup[2]
+    userTuple = getListenHistory()
+    listenHistory = userTuple[0]
+    username = userTuple[1]
+    userUrl = userTuple[2]
   result = buildHtml(main()):
     verbatim("<div id = 'mirror'><p>You are mirroring <a href='" & userUrl & "'>" & username & "</a>!</p></div>")
     tdiv(class = "listens"):
       ul:
-        if isSome(user.playingNow):
-          li(class = "listen"):
-            img(src = "/src/templates/assets/listening.svg")
-            tdiv(id = "listen-details"):
-              tdiv(id = "track-details"):
-                p(id = "track-name"):
-                  text get(user.playingNow).trackName
-                p(id = "artist-name"):
-                  text get(user.playingNow).artistName
-              span:
-                text "Playing now"
-        for track in user.listenHistory:
+      #   if isSome(user.playingNow):
+      #     li(class = "listen"):
+      #       img(src = "/src/templates/assets/listening.svg")
+      #       tdiv(id = "listen-details"):
+      #         tdiv(id = "track-details"):
+      #           p(id = "track-name"):
+      #             text get(user.playingNow).trackName
+      #           p(id = "artist-name"):
+      #             text get(user.playingNow).artistName
+      #         span:
+      #           text "Playing now"
+        for track in listenHistory:
           li(class = "listen"):
             img(src = "/src/templates/assets/pre-mirror-listen.svg")
             tdiv(id = "listen-details"):
