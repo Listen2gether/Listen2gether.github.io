53200cc8b88a2fdee3eef8ca03f9ab5d5a453711
tandy1000 <tandy1000@cock.li>
2021-09-25 14:51:00 +0100

    add create tables call

--- src/listen2gether.nim
+++ src/listen2gether.nim
@@ -1,8 +1,9 @@
 import pkg/prologue
 import pkg/prologue/middlewares/staticfile
-import routes/urls
+import routes/urls, models
 
 let settings = newSettings(appName = "Listen2gether", port = Port(8080))
+createTables()
 var app = newApp(settings = settings)
 app.use(staticFileMiddleware("src/templates"))
 app.addRoute(urls.urlPatterns, "")
