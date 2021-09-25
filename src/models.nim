b83178180b1129fd0c354a3716086b30293e5189
tandy1000 <tandy1000@cock.li>
2021-09-25 14:50:10 +0100

    refactor database functionality

--- src/models.nim
+++ src/models.nim
@@ -1,62 +1,55 @@
-import norm / [model, sqlite]
-import frosty
-import types, sources / [lfm, lb]
-
+import pkg/norm/[model, pragmas, sqlite]
+import pkg/jsony
+import types
 
 let DBLOCATION = "listen2gether.db"
 
 
 type
   UserTable = ref object of Model
-    user: string
-
-  ListenTable = ref object of Model
-    listen: string
-    trackMetadata: TrackMetadataTable
-
-  TrackMetadataTable = ref object of Model
-    trackMetadata: string
+    userID* {.unique.}: string
+    user*: string
 
 
-func newUserTable(user = ""): UserTable =
-  UserTable(user: user)
-
-
-func newTrackMetadataTable(track = ""): TrackMetadataTable =
-  TrackMetadataTable(trackMetadata: track)
-
-
-func newListenTable(
-  listen = "",
-  track = newTrackMetadataTable()): ListenTable =
-  ListenTable(listen: listen, trackMetadata: track)
+func newUserTable(userID = "", user = ""): UserTable =
+  UserTable(userID: userID, user: user)
 
 
 proc openDbConn*(dbLocation = DBLOCATION): DbConn =
   result = open(dbLocation, "", "", "")
 
 
-proc insertTables*(db: DbConn) =
+proc createTables*(db: DbConn = openDbConn()) =
   db.createTables(newUserTable())
-  db.createTables(newListenTable())
-  db.createTables(newTrackMetadataTable())
 
 
-proc insertUser*(
-  db: DbConn,
-  user: User) =
-  var user = newUserTable(freeze(user))
-  db.insert(user)
+proc insertUserTable*(
+  user: User,
+  service: Service,
+  db: DbConn = openDbConn()) =
+  var userTable = newUserTable(user.services[service].baseUrl, user.toJson())
+  db.insert(userTable)
 
 
-proc insertListen*(
-  db: DbConn,
-  listen: Listen) =
-  var
-    trackMetadata = newTrackMetadataTable(freeze(listen.trackMetadata))
-    listen = newListenTable(freeze(listen), trackMetadata)
-  db.insert(trackMetadata)
-  db.insert(listen)
+proc selectUserTable(
+  user: User,
+  service: Service,
+  db: DbConn = openDbConn()): UserTable =
+  result = newUserTable()
+  db.select(result, "UserTable.userID = ?", user.services[service].baseUrl)
 
 
-#proc getListen*(db: DbConn, ): Listen =
+proc selectUser*(
+  user: User,
+  service: Service): User =
+  let userTable = selectUserTable(user, service)
+  result = fromJson(userTable.user, User)
+
+
+proc updateUserTable*(
+  user: User,
+  service: Service,
+  db: DbConn = openDbConn()) =
+  var userTable = selectUserTable(user, service, db)
+  userTable.user = user.toJson()
+  db.update(userTable)
