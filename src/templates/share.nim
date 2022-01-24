c505f6284aafbede56b50c58d38b64e0af20d802
tandy <tandy1000@cock.li>
2022-01-24 19:48:38 +0000

    add comments

--- src/templates/share.nim
+++ src/templates/share.nim
@@ -2,22 +2,23 @@ import pkg/karax/[karaxdsl, vdom]
 
 
 proc head*(): Vnode =
+  ## Produces HTML head to be used on all server side rendered pages.
   result = buildHtml(head):
     meta(charset="utf-8", name="viewport", content="width=device-width, initial-scale=1")
     title: text "Listen2gether"
     link(rel="icon", href="/public/assets/favicon_square.svg")
     link(rel="stylesheet", href="/public/css/style.css")
 
-
 proc headerSection*(): Vnode =
+  ## Produces header section to be used on all pages.
   result = buildHtml(header):
     a(class = "header", href = "/"):
       text "Listen"
       span: text "2"
       text "gether"
 
-
 proc footerSection*(): Vnode =
+  ## Produces footer section to be used on all pages.
   result = buildHtml(footer):
     a(href = "https://www.gnu.org/licenses/agpl-3.0.html"):
       img(src = "/public/assets/agpl.svg", id = "agpl", class = "icon", alt = "GNU AGPL icon")
