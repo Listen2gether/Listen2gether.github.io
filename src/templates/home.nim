184c3577b545a28868b24f1df889da8a3cb2a8b8
tandy <tandy1000@cock.li>
2022-01-24 19:12:22 +0000

    improve homepage

--- src/templates/home.nim
+++ src/templates/home.nim
@@ -1,14 +1,16 @@
-import pkg/karax/[karax, karaxdsl, vdom, kdom]
-import share
+import
+  pkg/karax/[karax, karaxdsl, vdom, kdom],
+  share
 
 proc onServiceToggleClick(ev: Event; n: VNode) =
+  ## Switches service toggle on click
   if getElementById("service_switch").checked:
     getElementById("token").style.display = "none"
   else:
     getElementById("token").style.display = "flex"
-  
 
 proc onTokenEnter(ev: Event; n: VNode) =
+  ## Routes to mirror page on token enter
   var service: string = ""
   let
     username = $getElementById("username_input").value
@@ -16,15 +18,20 @@ proc onTokenEnter(ev: Event; n: VNode) =
     serviceSwitch = getElementById("service_switch").checked
   if serviceSwitch:
     service = "lastfm"
+    window.location.href = cstring("/mirror/" & service & "/" & username)
   else:
     service = "listenbrainz"
-  window.location.href = cstring("/mirror/" & service & "/" & username & "?token=" & token)
-  
+    # let lb = AsyncListenBrainz()
+    # if token != "":
+    #   let validToken = await lb.validateLbToken(token)
+    #   if validToken:
+    window.location.href = cstring("/mirror/" & service & "/" & username & "?token=" & token)
 
 proc mainSection(): Vnode =
+  ## Generates main section for Home page.
   result = buildHtml(main()):
-    tdiv(class = "inner-grid"):
-      tdiv(id = "grid-1", class = "col"):
+    tdiv(class = "container"):
+      tdiv(id = "title-container", class = "col"):
         p(id = "title"):
           a(class = "header", href = "/"):
             text "Listen"
@@ -33,24 +40,40 @@ proc mainSection(): Vnode =
           text " is a website for listen parties."
         p(id = "subtitle"):
           text "Whether you're physically in the same room or not."
-      tdiv(id = "grid-2", class = "col"):
-        img(src = "/src/templates/assets/screenshot.png", id = "demo", alt = "Mirroring page")
-      tdiv(id = "grid-3", class = "col"):
+      tdiv(id = "login-container", class = "col"):
+        p(id = "body"):
+          text "Enter a username and select a service to start mirroring another user's listens."
         tdiv(id = "username", class = "textbox"):
-          input(`type` = "text", class = "textinput", id = "username_input", placeholder = "Enter username / room ID")
+          input(`type` = "text", class = "textinput", id = "username_input", placeholder = "Enter username")
           label(class = "switch"):
             input(`type` = "checkbox", id = "service_switch", oninput = onServiceToggleClick)
             span(class = "slider")
         tdiv(id = "token", class = "textbox"):
           input(`type` = "text", class = "textinput", id = "token_input", placeholder = "Enter your ListenBrainz token", onkeyupenter = onTokenEnter)
-      tdiv(id = "grid-4", class = "col"):
+    tdiv(class = "container"):
+      tdiv(id = "description-container", class = "col"):
         p(id = "body"):
-          text "Virtual listen parties are powered by Youtube and Spotify, and a Matrix chatroom."
-        p(id = "body"):
-          text "Enter a username and select a service to start mirroring another user's listens."
+          text "Virtual listen parties are powered by ListenBrainz and a Matrix chatroom."
+      tdiv(id = "logo-container", class = "col"):
+        a(href = "https://listenbrainz.org/",
+          img(
+            src = "/public/assets/listenbrainz-logo.svg",
+            id = "listenbrainz-logo",
+            class = "logo",
+            alt = "ListenBrainz.org logo"
+          )
+        )
+        a(href = "https://matrix.org/",
+          img(
+            src = "/public/assets/matrix-logo.svg",
+            id = "matrix-logo",
+            class = "logo",
+            alt = "Matrix.org logo",
+          )
+        )
 
 proc createDom(): VNode =
-  result = buildHtml(tdiv(class = "grid")):
+  result = buildHtml(tdiv()):
     headerSection()
     mainSection()
     footerSection()
