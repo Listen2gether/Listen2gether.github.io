4db06b6285c6226b9b72a9bf011d93b4ff1d7dc4
tandy <tandy1000@cock.li>
2022-01-24 15:15:55 +0000

    remove tools.nim, use new paths

--- src/templates/share.nim
+++ src/templates/share.nim
@@ -5,8 +5,8 @@ proc head*(): Vnode =
   result = buildHtml(head):
     meta(charset="utf-8", name="viewport", content="width=device-width, initial-scale=1")
     title: text "Listen2gether"
-    link(rel="icon", href="/src/templates/assets/favicon_square.svg")
-    link(rel="stylesheet", href="/src/templates/style.css")
+    link(rel="icon", href="/public/assets/favicon_square.svg")
+    link(rel="stylesheet", href="/public/css/style.css")
 
 
 proc headerSection*(): Vnode =
@@ -20,6 +20,6 @@ proc headerSection*(): Vnode =
 proc footerSection*(): Vnode =
   result = buildHtml(footer):
     a(href = "https://www.gnu.org/licenses/agpl-3.0.html"):
-      img(src = "/src/templates/assets/agpl.svg", id = "agpl", class = "icon", alt = "GNU AGPL icon")
+      img(src = "/public/assets/agpl.svg", id = "agpl", class = "icon", alt = "GNU AGPL icon")
     a(href = "https://github.com/Listen2gether/website"):
-      img(src = "/src/templates/assets/github-logo.svg", class = "icon", alt = "GitHub Repository")
+      img(src = "/public/assets/github-logo.svg", class = "icon", alt = "GitHub Repository")
