f92a1957e1cbcbfae81d6d81455c5b2e08319516
tandy <tandy1000@cock.li>
2022-02-17 11:02:19 +0000

    remove public from path for CI

--- src/templates/share.nim
+++ src/templates/share.nim
@@ -17,8 +17,8 @@ proc head*(): Vnode =
   result = buildHtml(head):
     meta(charset="utf-8", name="viewport", content="width=device-width, initial-scale=1")
     title: text "Listen2gether"
-    link(rel="icon", href="/public/assets/favicon_square.svg")
-    link(rel="stylesheet", href="/public/css/style.css")
+    link(rel="icon", href="/assets/favicon_square.svg")
+    link(rel="stylesheet", href="/css/style.css")
 
 proc headerSection*(): Vnode =
   ## Produces header section to be used on all pages.
@@ -33,12 +33,12 @@ proc loadingModal*(message: cstring): Vnode =
     tdiv(class = "col login-container"):
       p(id = "body"):
         text message
-      img(id = "spinner", src = "/public/assets/spinner.svg")
+      img(id = "spinner", src = "/assets/spinner.svg")
 
 proc footerSection*(): Vnode =
   ## Produces footer section to be used on all pages.
   result = buildHtml(footer):
     a(href = "https://www.gnu.org/licenses/agpl-3.0.html"):
-      img(src = "/public/assets/agpl.svg", id = "agpl", class = "icon", alt = "GNU AGPL icon")
+      img(src = "/assets/agpl.svg", id = "agpl", class = "icon", alt = "GNU AGPL icon")
     a(href = "https://github.com/Listen2gether/website"):
-      img(src = "/public/assets/github-logo.svg", class = "icon", alt = "GitHub Repository")
+      img(src = "/assets/github-logo.svg", class = "icon", alt = "GitHub Repository")
