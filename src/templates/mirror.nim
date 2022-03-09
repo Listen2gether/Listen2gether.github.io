86d167b0f01a50a42ebe2a08c3a0d18657367a73
tandy1000 <tandy1000@cock.li>
2022-03-09 15:52:14 +0000

    start mirror page

--- src/templates/mirror.nim
+++ src/templates/mirror.nim
@@ -1,86 +1,81 @@
 import
-  std/[options, times],
-  pkg/prologue,
   pkg/karax/[karaxdsl, vdom],
-  share,
-  ../sources/[lb, lfm],
-  ../types
+  std/[times, options],
+  ../sources/[lb],
+  ../types, share
 
 
 # poll every 30 seconds to get current track?
 # separate nim file that compiles to create js, triggers a page reload
-# proc
 
-proc mainSection(service: Service, user: User): Vnode =
-  ## Generates main section for Mirror page.
+
+proc renderListens*(playingNow: Option[Track], listenHistory: seq[Track], maxListens: int = 6): Vnode =
   var
-    username, userUrl, trackName, artistName: string
+    trackName, artistName: cstring
     preMirrorSplit: bool = false
-    maxListens: int = 7
 
-  case service:
-    of listenBrainzService:
-      username = $user.services[listenBrainzService].username
-      userUrl = lb.userBaseUrl & username
-    of lastFmService:
-      username = $user.services[lastFmService].username
-      userUrl = lfm.userBaseUrl & username
-
-  result = buildHtml(main()):
-    verbatim("<div id = 'mirror'><p>You are mirroring <a href='" & userUrl & "'>" & username & "</a>!</p></div>")
+  result = buildHtml:
     tdiv(class = "listens"):
       ul:
-        if isSome(user.playingNow):
+        if isSome playingNow:
           li(class = "row listen"):
             tdiv(id = "listen-details"):
               img(src = "/assets/nowplaying.svg")
               tdiv(id = "track-details"):
-                trackName = get(user.playingNow).trackName
-                artistName = get(user.playingNow).artistName
+                trackName = get(playingNow).trackName
+                artistName = get(playingNow).artistName
                 p(title = trackName, id = "track-name"):
                   text trackName
                 p(title = artistName, id = "artist-name"):
                   text artistName
               span:
                 text "Playing now"
-          maxListens = 6
-        for idx, track in user.listenHistory[0..maxListens]:
-          if isSome(track.preMirror):
-            if get(track.preMirror) == true and preMirrorSplit == false:
-              if idx == 0:
-                preMirrorSplit = true
-              else:
-                hr()
-                preMirrorSplit = true
-          li(class = "row listen"):
-            tdiv(id = "listen-details"):
-              if isSome(track.mirrored):
-                if get(track.mirrored):
-                  img(src = "/assets/mirrored.svg")
-                else:
-                  img(src = "/assets/pre-mirror.svg")
-              tdiv(id = "track-details"):
-                trackName = track.trackName
-                artistName = track.artistName
-                p(title = trackName, id = "track-name"):
-                  text trackName
-                p(title = artistName, id = "artist-name"):
-                  text artistName
-              span(title = fromUnix(get(track.listenedAt)).format("HH:mm:ss dd/MM/yy")):
-                text fromUnix(get(track.listenedAt)).format("HH:mm")
+        # for idx, track in listenHistory[0..maxListens]:
+        #   if isSome(track.preMirror):
+        #     if get(track.preMirror) == true and preMirrorSplit == false:
+        #       if idx == 0:
+        #         preMirrorSplit = true
+        #       else:
+        #         hr()
+        #         preMirrorSplit = true
+        #   li(class = "row listen"):
+        #     tdiv(id = "listen-details"):
+        #       if isSome(track.mirrored):
+        #         if get(track.mirrored):
+        #           img(src = "/assets/mirrored.svg")
+        #         else:
+        #           img(src = "/assets/pre-mirror.svg")
+        #       tdiv(id = "track-details"):
+        #         trackName = track.trackName
+        #         artistName = track.artistName
+        #         p(title = trackName, id = "track-name"):
+        #           text trackName
+        #         p(title = artistName, id = "artist-name"):
+        #           text artistName
+        #       let
+        #         date = cstring fromUnix(get(track.listenedAt)).format("HH:mm:ss dd/MM/yy")
+        #         time = fromUnix(get(track.listenedAt)).format("HH:mm")
+        #       span(title = date):
+        #         text time
 
-proc mirrorPage*(ctx: Context, service: Service, user: User): string =
-  ## Generates Mirror page.
-  let
-    head = head()
-    header = headerSection()
-    main = mainSection(service, user)
-    footer = footerSection()
-    vnode = buildHtml(html):
-      head
-      body:
-        tdiv(id = "ROOT"):
-          header
-          main
-          footer
-  result = $vnode
+proc mainSection*(user: User, service: Service): Vnode =
+  var username, userUrl: cstring
+
+  if user.isNil:
+    echo "oh no!"
+  case service:
+  of Service.listenBrainzService:
+    username = user.services[Service.listenBrainzService].username
+    userUrl = cstring(lb.userBaseUrl & $username)
+  of Service.lastFmService:
+    username = user.services[Service.lastFmService].username
+    # userUrl = lfm.userBaseUrl & username
+
+  result = buildHtml:
+    main:
+      tdiv(id = "mirror"):
+        p:
+          text "You are mirroring "
+          a(href = userUrl):
+            text $username & "!"
+      renderListens(user.playingNow, user.listenHistory)
