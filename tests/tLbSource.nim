139bac0b4e593ef217d19124f155e6076645ed1d
tandy <tandy1000@cock.li>
2022-03-14 16:00:00 +0000

    add tests for lb source

---
+++ tests/tLbSource.nim
@@ -0,0 +1,76 @@
+import std/unittest
+include ../src/sources/lb
+
+suite "ListenBrainz source":
+
+  suite "to Helpers":
+    test "Convert some `Option[seq[cstring]]` to `Option[seq[string]]`":
+      let
+        cstringSeq: Option[seq[cstring]] = some @[cstring "test!", cstring "test?"]
+        stringSeq: Option[seq[string]] = some @["test!", "test?"]
+      check to(cstringSeq) == stringSeq
+
+    test "Convert none `Option[seq[cstring]]` to `Option[seq[string]]`":
+      let
+        cstringSeq: Option[seq[cstring]] = none seq[cstring]
+        stringSeq: Option[seq[string]] = none seq[string]
+      check to(cstringSeq) == stringSeq
+
+    test "Convert some `Option[seq[string]]` to `Option[seq[cstring]]`":
+      let
+        stringSeq: Option[seq[string]] = some @["test!", "test?"]
+        cstringSeq: Option[seq[cstring]] = some @[cstring "test!", cstring "test?"]
+      check to(stringSeq) == cstringSeq
+
+    test "Convert none `Option[seq[string]]` to `Option[seq[cstring]]`":
+      let
+        stringSeq: Option[seq[string]] = none seq[string]
+        cstringSeq: Option[seq[cstring]] = none seq[cstring]
+      check to(stringSeq) == cstringSeq
+
+    test "Convert some `Option[string]` to `Option[cstring]`":
+      let
+        str: Option[string] = some "test!"
+        cstr: Option[cstring] = some cstring "test!"
+      check to(str) == cstr
+
+    test "Convert none `Option[string]` to `Option[cstring]`":
+      let
+        stringSeq: Option[string] = none string
+        cstringSeq: Option[cstring] = none cstring
+      check to(stringSeq) == cstringSeq
+
+    test "Convert `Track` to `APIListen` (Simple)":
+      let
+        trackName = "track"
+        artistName = "artist"
+        track = newTrack(cstring trackName, cstring artistName)
+        listenedAt = some 1
+        apiListen = newAPIListen(listenedAt = listenedAt, trackMetadata = newTrackMetadata(trackName, artistName))
+        newAPIListen = to(track, listenedAt)
+      check newAPIListen.listenedAt == apiListen.listenedAt and newAPIListen.trackMetadata.trackName == apiListen.trackMetadata.trackName and newAPIListen.trackMetadata.artistName == apiListen.trackMetadata.artistName
+
+    test "Convert `APIListen` to `Track` (Simple)":
+      let
+        trackName = "track"
+        artistName = "artist"
+        apiListen = newAPIListen(trackMetadata = newTrackMetadata(trackName, artistName))
+        preMirror = some true
+        track = newTrack(cstring trackName, cstring artistName, preMirror = preMirror)
+        newTrack = to(apiListen, preMirror)
+      check newTrack.trackName == track.trackName and newTrack.artistName == track.artistName and newTrack.preMirror == track.preMirror
+
+    test "Convert `seq[APIListen]` to `seq[Track]` (Simple)":
+      let
+        apiListens = @[newAPIListen(trackMetadata = newTrackMetadata("track", "artist")), newAPIListen(trackMetadata = newTrackMetadata("track1", "artist1"))]
+        tracks = @[newTrack(cstring "track", cstring "artist", mirrored = some false), newTrack(cstring "track1", cstring "artist1", mirrored = some false)]
+        newTracks = to(apiListens)
+      check newTracks == tracks
+
+  suite "API tools":
+    test "diffTracks concat (Simple)":
+      let
+        oldTracks = @[newTrack(cstring "track", cstring "artist", listenedAt = some 2), newTrack(cstring "track", cstring "artist", listenedAt = some 1)]
+        tracks = @[newTrack(cstring "track", cstring "artist", listenedAt = some 4), newTrack(cstring "track", cstring "artist", listenedAt = some 3)]
+        newTracks = tracks & oldTracks
+      check diffTracks(tracks, oldTracks) == newTracks
