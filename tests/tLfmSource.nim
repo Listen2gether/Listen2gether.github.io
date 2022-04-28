ca928b3eed587d8bc2230b2e4c9900baf0bafa39
tandy <tandy1000@cock.li>
2022-04-28 14:47:37 +0100

    simplification

--- tests/tLfmSource.nim
+++ tests/tLfmSource.nim
@@ -1,18 +1,36 @@
-import std/[unittest, asyncdispatch, options, json]
+import std/unittest
 include ../src/sources/lfm
 
 suite "Last.FM source":
 
   suite "Helpers":
-    let track: FMTrack = newFMTrack(
-      artist = parseJson """{"mbid": "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8", "#text": "Soichi Terada"}""",
-      album = parseJson """{"mbid": "e07d5174-6181-498f-88fb-b92833bb81de", "#text": "Acid Face"}""",
-      date = some FMDate(uts: "0", text: ""),
-      mbid = some "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8",
-      name = some "鳴門海峡",
-      url = some "https://www.last.fm/music/Soichi+Terada/_/%E9%B3%B4%E9%96%80%E6%B5%B7%E5%B3%A1",
-      `@attr` = none Attributes
-    )
+    let
+      scrobble = newScrobble(
+        track = "鳴門海峡",
+        artist = "Soichi Terada",
+        album = some "Acid Face",
+        mbid = some "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8",
+        albumArtist = some "Soichi Terada",
+        timestamp = some 0,
+        trackNumber = some 3,
+      )
+      track: FMTrack = newFMTrack(
+        artist = parseJson """{"mbid": "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8", "#text": "Soichi Terada"}""",
+        album = parseJson """{"mbid": "e07d5174-6181-498f-88fb-b92833bb81de", "#text": "Acid Face"}""",
+        date = some FMDate(uts: "0", text: ""),
+        mbid = scrobble.mbid,
+        name = some scrobble.track,
+      )
+      listen = newListen(
+        trackName = cstring get track.name,
+        artistName = get getVal(track.artist, "#text"),
+        releaseName = getVal(track.album, "#text"),
+        recordingMbid = to track.mbid,
+        releaseMbid = getVal(track.album, "mbid"),
+        artistMbids = parseMbids getStr track.artist{"mbid"},
+        trackNumber = scrobble.trackNumber,
+        listenedAt = parseDate track.date,
+      )
 
     test "Convert JsonNode to Option[cstring]":
       check getVal(track.artist, "#text") == some(cstring "Soichi Terada")
@@ -24,13 +42,18 @@ suite "Last.FM source":
       check parseMbids(get track.mbid) == some(@[cstring "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8"])
 
     test "Convert FMTrack to Listen":
-      let listen = newListen(
-        trackName = cstring get track.name,
-        artistName = get getVal(track.artist, "#text"),
-        releaseName = getVal(track.album, "#text"),
-        recordingMbid = to track.mbid,
-        releaseMbid = getVal(track.album, "mbid"),
-        artistMbids = parseMbids getStr track.artist{"mbid"},
-        listenedAt = parseDate track.date
-      )
-      check to(track) == listen
+      var trackListen = listen
+      trackListen.trackNumber = none int
+      check to(track) == trackListen
+
+    test "Convert seq[FMTrack] to seq[Listen]":
+      var trackListen = listen
+      trackListen.trackNumber = none int
+      check to(@[track, track]) == @[trackListen, trackListen]
+
+    test "Convert Scrobble to Listen":
+      var scrobListen = listen
+      scrobListen.recordingMbid = none cstring
+      scrobListen.releaseMbid = none cstring
+      check to(scrobble) == scrobListen
+
