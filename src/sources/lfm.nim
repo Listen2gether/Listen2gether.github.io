8afbc0a9a1499c7db1eb86c383263c17353154ed
tandy <tandy1000@cock.li>
2022-04-28 15:01:45 +0100

    whitespace

--- src/sources/lfm.nim
+++ src/sources/lfm.nim
@@ -170,7 +170,6 @@ proc scrobbleTrack(
   except:
     logError "There was a problem scrobbling " & scrobble.track & " - " & scrobble.artist & "!"
 
-
 proc scrobbleTracks(
   fm: AsyncLastFM,
   scrobbles: seq[Scrobble]): Future[seq[JsonNode]] {.async.} =
