9f945151d8299d0ccabbf1ba3aabbfb02ffc4a48
tandy <tandy1000@cock.li>
2022-04-29 13:47:56 +0100

    use setup

--- tests/tUtils.nim
+++ tests/tUtils.nim
@@ -2,50 +2,37 @@ import std/[unittest, options]
 import ../src/sources/utils
 
 suite "Utils":
-    test "Convert some `Option[seq[cstring]]` to `Option[seq[string]]`":
+    setup:
       let
-        cstringSeq: Option[seq[cstring]] = some @[cstring "test!", cstring "test?"]
-        stringSeq: Option[seq[string]] = some @["test!", "test?"]
-      check to(cstringSeq) == stringSeq
+        someCstrSeq: Option[seq[cstring]] = some @[cstring "test!", cstring "test?"]
+        someStrSeq: Option[seq[string]] = some @["test!", "test?"]
+        noneCstrSeq: Option[seq[cstring]] = none seq[cstring]
+        noneStrSeq: Option[seq[string]] = none seq[string]
+        someCstr: Option[cstring] = some cstring ""
+        someStr: Option[string] = some ""
+        noneCstr: Option[cstring] = none cstring
+        noneStr: Option[string] = none string
+
+    test "Convert some `Option[seq[cstring]]` to `Option[seq[string]]`":
+      check to(someCstrSeq) == someStrSeq
 
     test "Convert none `Option[seq[cstring]]` to `Option[seq[string]]`":
-      let
-        cstringSeq: Option[seq[cstring]] = none seq[cstring]
-        stringSeq: Option[seq[string]] = none seq[string]
-      check to(cstringSeq) == stringSeq
+      check to(noneCstrSeq) == noneStrSeq
 
     test "Convert some `Option[seq[string]]` to `Option[seq[cstring]]`":
-      let
-        stringSeq: Option[seq[string]] = some @["test!", "test?"]
-        cstringSeq: Option[seq[cstring]] = some @[cstring "test!", cstring "test?"]
-      check to(stringSeq) == cstringSeq
+      check to(someStrSeq) == someCstrSeq
 
     test "Convert none `Option[seq[string]]` to `Option[seq[cstring]]`":
-      let
-        stringSeq: Option[seq[string]] = none seq[string]
-        cstringSeq: Option[seq[cstring]] = none seq[cstring]
-      check to(stringSeq) == cstringSeq
+      check to(noneStrSeq) == noneCstrSeq
 
     test "Convert some `Option[string]` to `Option[cstring]`":
-      let
-        str: Option[string] = some "test!"
-        cstr: Option[cstring] = some cstring "test!"
-      check to(str) == cstr
+      check to(someStr) == someCstr
 
     test "Convert none `Option[string]` to `Option[cstring]`":
-      let
-        str: Option[string] = none string
-        cstr: Option[cstring] = none cstring
-      check to(str) == cstr
+      check to(noneStr) == noneCstr
 
     test "Convert some `Option[cstring]` to `Option[string]`":
-      let
-        cstr: Option[cstring] = some cstring "test!"
-        str: Option[string] = some "test!"
-      check to(cstr) == str
+      check to(someCstr) == someStr
 
     test "Convert none `Option[cstring]` to `Option[string]`":
-      let
-        cstr: Option[cstring] = none cstring
-        str: Option[string] = none string
-      check to(cstr) == str
+      check to(noneCstr) == noneStr
