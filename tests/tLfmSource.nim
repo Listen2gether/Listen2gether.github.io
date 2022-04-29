62846519411d3f4d4d57f679ec63b7426f8fe3af
tandy <tandy1000@cock.li>
2022-04-29 13:37:45 +0100

    better tests

--- tests/tLfmSource.nim
+++ tests/tLfmSource.nim
@@ -9,59 +9,62 @@ suite "Last.FM source":
         track = "鳴門海峡",
         artist = "Soichi Terada",
         album = some "Acid Face",
-        mbid = some "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8",
+        mbid = some "recording-mbid",
         albumArtist = some "Soichi Terada",
         timestamp = some 0,
         trackNumber = some 3,
       )
       track: FMTrack = newFMTrack(
-        artist = parseJson """{"mbid": "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8", "#text": "Soichi Terada"}""",
-        album = parseJson """{"mbid": "e07d5174-6181-498f-88fb-b92833bb81de", "#text": "Acid Face"}""",
+        artist = parseJson """{"mbid": "artist-mbid", "#text": "Soichi Terada"}""",
+        album = parseJson """{"mbid": "album-mbid", "#text": "Acid Face"}""",
         date = some FMDate(uts: "0", text: ""),
         mbid = scrobble.mbid,
         name = some scrobble.track,
       )
       listen = newListen(
         trackName = cstring get track.name,
-        artistName = get getVal(track.artist, "#text"),
-        releaseName = getVal(track.album, "#text"),
+        artistName = cstring "Soichi Terada",
+        releaseName = some cstring "Acid Face",
         recordingMbid = to track.mbid,
-        releaseMbid = getVal(track.album, "mbid"),
-        artistMbids = parseMbids getStr track.artist{"mbid"},
+        releaseMbid = some cstring "release-mbid",
+        artistMbids = some @[cstring "artist-mbid"],
         trackNumber = scrobble.trackNumber,
-        listenedAt = parseDate track.date,
+        listenedAt = scrobble.timestamp,
       )
 
-    test "Convert JsonNode to Option[cstring]":
+    test "Convert `JsonNode` to `Option[cstring]`":
       check getVal(track.artist, "#text") == some(cstring "Soichi Terada")
 
-    test "Convert FMDate to Option[int]":
+    test "Convert `FMDate` to `Option[int]`":
       check parseDate(track.date) == some(0)
 
-    test "Convert Mbids to Option[seq[cstring]]":
-      check parseMbids(get track.mbid) == some(@[cstring "fabc9908-3a4a-4c79-86bd-1f7e4506b0d8"])
+    test "Convert `string` to `Option[seq[cstring]]`":
+      check parseMbids(get track.mbid) == some(@[cstring "recording-mbid"])
 
-    test "Convert FMTrack to Listen":
-      var trackListen = listen
-      trackListen.trackNumber = none int
-      check to(track) == trackListen
+    test "Convert `FMTrack` to `Listen`":
+      var newListen = listen
+      newListen.trackNumber = none int
+      check to(track) == newListen
 
-    test "Convert seq[FMTrack] to seq[Listen]":
+    test "Convert `seq[FMTrack]` to `seq[Listen]`":
       var trackListen = listen
       trackListen.trackNumber = none int
       check to(@[track, track]) == @[trackListen, trackListen]
 
-    test "Convert Scrobble to Listen":
-      var scrobListen = listen
-      scrobListen.recordingMbid = none cstring
-      scrobListen.releaseMbid = none cstring
-      check to(scrobble) == scrobListen
+    test "Convert `Scrobble` to `Listen`":
+      var newListen = listen
+      newListen.releaseMbid = none cstring
+      newListen.artistMbids = none seq[cstring]
+      check to(scrobble) == newListen
+
+    test "Convert `Scrobble` to `Listen` to `Scrobble`":
+      check scrobble == to to scrobble
 
     test "Convert seq[Scrobble] to seq[Listen]":
-      var scrobListen = listen
-      scrobListen.recordingMbid = none cstring
-      scrobListen.releaseMbid = none cstring
-      check to(@[scrobble, scrobble]) == @[scrobListen, scrobListen]
+      var newListen = listen
+      newListen.releaseMbid = none cstring
+      newListen.artistMbids = none seq[cstring]
+      check to(@[scrobble, scrobble]) == @[newListen, newListen]
 
   suite "API tools":
     setup:
