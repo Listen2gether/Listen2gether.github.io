49f3eca0ae6ceb09d3ef0f04e7e6c7485caab0e5
tandy1000 <tandy1000@cock.li>
2022-09-02 00:05:39 +0100

    use `latestListenTs` instead of `lastUpdateTs`

--- src/sources/pure.nim
+++ src/sources/pure.nim
@@ -4,13 +4,13 @@ import
 
 proc queueFilter(
   listen: Listen,
-  lastUpdateTs: int,
-  lastSubmissionTs: Option[int]): bool =
-  ## Returns true when a Listen is either newer than `lastUpdateTs` or `lastSubmissionTs`.
+  latestListenTs, lastSubmissionTs: Option[int]): bool =
+  ## Returns true when a Listen is either newer than `latestListenTs` or `lastSubmissionTs`.
   if listen.listenedAt.isSome():
     let listenedAt = get(listen.listenedAt)
-    if listenedAt > lastUpdateTs:
-      result = true
+    if latestListenTs.isSome():
+      if listenedAt > get(latestListenTs):
+        result = true
     if lastSubmissionTs.isSome():
       if listenedAt > get(lastSubmissionTs):
         result = true
@@ -19,9 +19,8 @@ proc updateQueue*(users: seq[User], mirror: User): seq[User] =
   ## Updates the `submitQueue` for all users in `seq[User]`, given a mirror user.
   for user in users:
     var updatedUser = user
-    let queue = collect(newSeq):
+    updatedUser.submitQueue.listens = collect(newSeq):
       for listen in mirror.listenHistory:
-        if queueFilter(listen, updatedUser.lastUpdateTs, updatedUser.lastSubmissionTs):
+        if queueFilter(listen, updatedUser.listenHistory[0].listenedAt, updatedUser.lastSubmissionTs):
           listen
-    updatedUser.submitQueue.listens = queue
     result.add updatedUser
