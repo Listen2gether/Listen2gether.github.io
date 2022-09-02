3853f4d4a3cecc1c7396e377bfc09569bdb9540b
tandy1000 <tandy1000@cock.li>
2022-09-01 17:34:25 -0700

    refactor

--- src/sources/pure.nim
+++ src/sources/pure.nim
@@ -2,7 +2,7 @@ import
   std/[sugar, options],
   types
 
-proc queueFilter(
+proc queueFilter*(
   listen: Listen,
   latestListenTs, lastSubmissionTs: Option[int]): bool =
   ## Returns true when a Listen is either newer than `latestListenTs` or `lastSubmissionTs`.
@@ -14,13 +14,22 @@ proc queueFilter(
     if lastSubmissionTs.isSome():
       if listenedAt > get(lastSubmissionTs):
         result = true
+      else:
+        result = false
+  else:
+    raise newException(IOError, "Listen has no listenedAt property!")
+
+proc updateQueue*(user: User, mirror: User): seq[Listen] =
+  ## Updates the `submitQueue` for a user given a mirror user.
+  result = collect():
+    for listen in mirror.listenHistory:
+      if queueFilter(listen, user.listenHistory[0].listenedAt, user.lastSubmissionTs):
+        listen
 
 proc updateQueue*(users: seq[User], mirror: User): seq[User] =
   ## Updates the `submitQueue` for all users in `seq[User]`, given a mirror user.
-  for user in users:
-    var updatedUser = user
-    updatedUser.submitQueue.listens = collect(newSeq):
-      for listen in mirror.listenHistory:
-        if queueFilter(listen, updatedUser.listenHistory[0].listenedAt, updatedUser.lastSubmissionTs):
-          listen
-    result.add updatedUser
+  result = collect():
+    for user in users:
+      var updatedUser = user
+      updatedUser.submitQueue.listens = updateQueue(user, mirror)
+      updatedUser
